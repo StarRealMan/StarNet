10f6cda0c8b2ddca0c0993b5f68597b18da64a04
StarRealMan <965669036@qq.com>
2020-12-08 22:46:48 +0800

    Fix dataloader bug

--- app/dataloader.py
+++ app/dataloader.py
@@ -10,23 +10,26 @@ from tqdm import tqdm
 class S3DISDataset(data.Dataset):
     def __init__(self,root_d):
         self.room_names = []
-        self.label_names = ['ceiling','floor','wall','beam','column','window','door','table','chair','sofa','bookcase','board','clutter']
+        self.label_names = ['ceiling','floor','wall','beam','column','window','door','stairs','table','chair','sofa','bookcase','board','clutter']
         self.label_codes = {}
-        self.data = []
+        self.points = []
         self.label = []
+        room_data = []
+        room_label = []
+        line_data = []
 
         for label_num, label_name_e in enumerate(self.label_names):
             self.label_codes[label_name_e] = label_num
 
-        for Area_num in range(6):
+        for Area_num in range(1):
             for root,dirs,files in os.walk(root_d+'/Area_'+str(Area_num+1)):
                 root_split = root.split('/')
                 if(root_split[-1] == 'Annotations'):
                     room_name = root_split[-2]
                     self.room_names.append('Area_'+str(Area_num+1)+room_name)
                     
-                    room_data = []
-                    room_label = []
+                    room_data.clear()
+                    room_label.clear()
                     for file in files:
                         label_name = file.split('_')[0]
                         if(label_name != 'Icon'):
@@ -34,22 +37,42 @@ class S3DISDataset(data.Dataset):
                             with open(root_d+'/Area_'+str(Area_num+1)+'/'+room_name+'/Annotations/'+file, 'r') as f:                                
                                 lines = f.readlines()
                                 for line in lines:
-                                    room_data.append(line.split(' '))
+                                    line_data = line.split(' ')
+                                    line_data[0] = float(line_data[0])
+                                    line_data[1] = float(line_data[1])
+                                    line_data[2] = float(line_data[2])
+                                    line_data[3] = int(float(line_data[3]))
+                                    line_data[4] = int(float(line_data[4]))
+                                    line_data[5] = int(float(line_data[5]))
+                                    
+                                    room_data.append(line_data)
                                     room_label.append(self.label_codes[label_name])
 
-                            self.data.append(room_data)
+                            self.points.append(room_data)
                             self.label.append(room_label)
 
         
     def __getitem__(self, index):
-        self.data[index]
-        self.label[index]
-
-        return , 
+        
+        points = np.array(self.points[index])
+        label = np.array(self.label[index])
+        
+        return points, label
         
     def __len__(self):
         
-        return self.room_names.size()
+        return len(self.room_names)
 
 if __name__ == '__main__':
-    dataset = S3DISDataset('../data/Stanford3dDataset_v1.2_Aligned_Version')
+    # dataset = S3DISDataset('../data/Stanford3dDataset_v1.2_Aligned_Version')
+    dataset = S3DISDataset('../data/test_dataset')
+    dataloader = torch.utils.data.DataLoader(dataset,shuffle=True,batch_size=1,num_workers=4)
+
+    for i, data in enumerate(dataloader):
+        points,label = data
+        print(points)
+        print(label)
+        break;
+
+    print(dataset.__getitem__(0))
+    print(dataset.__len__())
--- data/removedata.sh
+++ data/removedata.sh
@@ -3,3 +3,7 @@
 echo "Remove all pcd data!"
 
 rm ./savings/*.pcd
+
+echo "Remove all .DS_Store file!"
+
+find . -name '*.DS_Store' -type f -delete
