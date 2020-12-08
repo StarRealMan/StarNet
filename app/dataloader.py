39b3a6df10c6388d8a2650028625cacca1eea942
StarRealMan <965669036@qq.com>
2020-12-08 17:21:38 +0800

    Update Dataloader

--- app/dataloader.py
+++ app/dataloader.py
@@ -9,38 +9,47 @@ from tqdm import tqdm
 
 class S3DISDataset(data.Dataset):
     def __init__(self,root_d):
+        self.room_names = []
+        self.label_names = ['ceiling','floor','wall','beam','column','window','door','table','chair','sofa','bookcase','board','clutter']
+        self.label_codes = {}
+        self.data = []
+        self.label = []
+
+        for label_num, label_name_e in enumerate(self.label_names):
+            self.label_codes[label_name_e] = label_num
 
         for Area_num in range(6):
-            for root,dirs,files in os.walk(root_d):
-                print('root:',root)
-                print('dirs:',dirs)
-                print('files:',files)
-                
+            for root,dirs,files in os.walk(root_d+'/Area_'+str(Area_num+1)):
+                root_split = root.split('/')
+                if(root_split[-1] == 'Annotations'):
+                    room_name = root_split[-2]
+                    self.room_names.append('Area_'+str(Area_num+1)+room_name)
+                    
+                    room_data = []
+                    room_label = []
+                    for file in files:
+                        label_name = file.split('_')[0]
+                        if(label_name != 'Icon'):
 
+                            with open(root_d+'/Area_'+str(Area_num+1)+'/'+room_name+'/Annotations/'+file, 'r') as f:                                
+                                lines = f.readlines()
+                                for line in lines:
+                                    room_data.append(line.split(' '))
+                                    room_label.append(self.label_codes[label_name])
 
-                
-        # with open(root+'Area_2') as Farea2:
-        #     for line in Farea2:
+                            self.data.append(room_data)
+                            self.label.append(room_label)
 
-        # with open(root+'Area_3') as Farea3:
-        #     for line in Farea3:
-
-        # with open(root+'Area_4') as Farea4:
-        #     for line in Farea4:
-
-        # with open(root+'Area_5') as Farea5:
-        #     for line in Farea5:
-
-        # with open(root+'Area_6') as Farea6:
-        #     for line in Farea6:
         
     def __getitem__(self, index):
-        
-        return label, image
+        self.data[index]
+        self.label[index]
+
+        return , 
         
     def __len__(self):
         
-        return len
+        return self.room_names.size()
 
 if __name__ == '__main__':
-    dataset = S3DISDataset('../data/Stanford3dDataset_v1.2_Aligned_Version/Area_1')
+    dataset = S3DISDataset('../data/Stanford3dDataset_v1.2_Aligned_Version')
