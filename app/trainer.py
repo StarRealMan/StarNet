3614d988166d96bbaa5873b4612f4bea0af3eaca
StarRealMan <965669036@qq.com>
2020-12-09 15:48:26 +0800

    Update Net and train

--- app/trainer.py
+++ app/trainer.py
@@ -1,8 +1,6 @@
 import sys
 sys.path.append("..")
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import argparse
 from tqdm import tqdm
 
@@ -17,21 +15,22 @@ parser.add_argument('--outf', type=str, default='model.pt', help='output folder'
 parser.add_argument('--modelf', type=str, default='None', help='model path')
 parser.add_argument('--workers', type=int, default=2, help='number of workers to load data')
 
-
 opt = parser.parse_args()
 print(opt)
 
-dataset = 
-dataloader = torch.utils.data.DataLoader(dataset,shuffle=True,batch_size=opt.batchsize,num_workers=opt.workers)
+train_dataset = dataloader.S3DISDataset('../data/test_dataset')
+traindataloader = torch.utils.data.DataLoader(train_dataset,shuffle=True,batch_size=opt.batchsize,num_workers=opt.workers)
 
-val_dataset = 
-valdataloader = torch.utils.data.DataLoader(val_dataset,shuffle=True,batch_size=opt.batchsize,num_workers=opt.workers)
+# val_dataset = 
+# valdataloader = torch.utils.data.DataLoader(val_dataset,shuffle=True,batch_size=opt.batchsize,num_workers=opt.workers)
 
-print('length of dataset: %s ; length of val dataset: %s' % (len(dataset), len(val_dataset)))
-num_batch = len(dataset) / opt.batchsize
+print('length of dataset: %s' % (len(train_dataset)))
+num_batch = len(train_dataset) / opt.batchsize
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-model = 
-model.cuda()
+
+# train process
+model = StarNet.Net()
 
 if opt.modelf != 'None':
     model.load_state_dict(torch.load('../model'+opt.modelf))
@@ -39,27 +38,22 @@ if opt.modelf != 'None':
 else:
     print('Use new model')
 
+model.to(device)
 optimizer = torch.optim.Adam(model.parameters(),lr=0.0001,betas=(0.9, 0.999))
 
 for epoch in tqdm(range(opt.nepoch)):
     show_loss = 0
     for i, data in tqdm(enumerate(dataloader)):
         label,image = data
-        image = (image.float())/255.0
-        label,image = label.cuda(),image.cuda()
+        label,image = label.to(device),image.to(device)
         optimizer.zero_grad()
         model = model.train()
         pred = model(image)
-        label_vec = torch.zeros(pred.size())
-        for iterate in range(opt.batchsize):
-            label_vec[iterate][label[iterate]] = 1
-        label_vec = label_vec.cuda()
-        loss = F.pairwise_distance(label_vec,pred,p=2)
-        loss = loss.sum()
+        loss = 
         loss.backward()
         optimizer.step()
         show_loss = show_loss + loss.item()
-        if i % 10 == 0:
+        if i % 100 == 0:
             # testdata
             print('[ epoch: %d  batch: %d/%d ]  loss: %f' % (epoch,i,num_batch,show_loss,))
             show_loss = 0
