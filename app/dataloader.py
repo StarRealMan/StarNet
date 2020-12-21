b6188161a2a70ab57674dddde76163cac369bdfb
StarRealMan <965669036@qq.com>
2020-12-21 14:44:09 +0800

    Fix dataset Bug

--- app/dataloader.py
+++ app/dataloader.py
@@ -8,7 +8,8 @@ import cv2
 class S3DISDataset(data.Dataset):
     def __init__(self,root_d):
         self.room_names = []
-        self.label_names = ['ceiling','floor','wall','beam','column','window','door','stairs','table','chair','sofa','bookcase','board','clutter']
+        self.label_names = ['ceiling','floor','wall','beam','column','window','door',\
+                            'stairs','table','chair','sofa','bookcase','board','clutter']
         self.label_codes = {}
         self.points = []
         self.label = []
@@ -19,11 +20,13 @@ class S3DISDataset(data.Dataset):
         for label_num, label_name_e in enumerate(self.label_names):
             self.label_codes[label_name_e] = label_num
 
-        for Area_num in range(6):
+        for Area_num in range(4,6):
+            print("loading Area_" + str(Area_num+1))
             for root,dirs,files in os.walk(root_d+'/Area_'+str(Area_num+1)):
                 root_split = root.split('/')
                 if(root_split[-1] == 'Annotations'):
                     room_name = root_split[-2]
+                    print("loading Area_" + str(Area_num+1) +" "+ room_name)
                     self.room_names.append('Area_'+str(Area_num+1)+room_name)
                     
                     room_data.clear()
@@ -31,7 +34,7 @@ class S3DISDataset(data.Dataset):
                     for file in files:
                         label_name = file.split('_')[0]
                         if(label_name != 'Icon'):
-
+                            print("loading Area_" + str(Area_num+1) +" "+ room_name +" "+ file)
                             with open(root_d+'/Area_'+str(Area_num+1)+'/'+room_name+'/Annotations/'+file, 'r') as f:                                
                                 lines = f.readlines()
                                 for line in lines:
